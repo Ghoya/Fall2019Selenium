c2b2442bc8eeb954f80689f6cbf49a783b8708cc
Ghoya <canadamubarak1@gmail.com>
2020-03-04 12:41:03 -0800

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -16,6 +16,8 @@ public class BasicNavigation {
         WebDriver driver = new ChromeDriver();// we will use Chrom
 // in Selenium every thing began with WebDriver interface
         driver.get("http://google.com"); // to open a webside
+      driver.manage().window().maximize();// maximizing the window of chrome
+   //     driver.manage().window().fullscreen();
         Thread.sleep(3000);// for demo , waite 3 seconds
         // method that return page title
         // you can also see it as tab name , in the browser
@@ -26,8 +28,41 @@ public class BasicNavigation {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            driver.close();// to close browser
         }
-            // browser cann't close it self
+  /// go to another webside within the same window
+   //     driver.navigate().to() is same with  driver.get()
+driver.navigate().to("http://amazon.com");
+        Thread.sleep(3000);// for demo , waite 3 seconds
+if (driver.getTitle().toLowerCase().contains("amazon")){
+    System.out.println("Test passed");
+}else {
+    System.out.println("Test failed");
+}
+// come back to Google
+driver.navigate().back();
+        Thread.sleep(3000);// for demo , waite 3 seconds
+// checking if page title is equals to Google
+        verifyEquals(driver.getTitle(), "Google");
+        // .get title() return page title
+
+        driver.navigate().forward();// move forward in amazon history
+        Thread.sleep(3000);// for demo , waite 3 seconds
+        // again go back to amazon
+driver.navigate().refresh();
+        Thread.sleep(3000);// for demo , waite 3 seconds
+        System.out.println("Title = "+driver.getTitle());
+// must be at the end
+driver.close();// to close browser
+        // browser cann't close it self
         }
+
+
+ public static void verifyEquals(String arg1, String arg2){
+        if (arg1.equals(arg2)){
+            System.out.println("Test passed");
+        }else {
+            System.out.println("Test failed");
+        }
+ }
+
     }
