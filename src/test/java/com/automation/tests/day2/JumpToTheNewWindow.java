fdb6c0b9868e38fb4069982cd9606ad06b0725ba
Ghoya <canadamubarak1@gmail.com>
2020-03-04 14:41:11 -0800

    created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -31,4 +31,15 @@ Set<String> windowHandles=driver.getWindowHandles();
         System.out.println("After Switch : " + driver.getCurrentUrl());
         driver.close();
     }
+    // This method helps to switch in between windows based on page title
+  public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String> windows = driver.getWindowHandles();
+        for(String window: windows){
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(pageTitle)){
+                break;
+            }
+        }
+  }
+
 }
