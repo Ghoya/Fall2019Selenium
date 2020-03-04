98d6b6670ff604fd4e53dfe7b775eaf6fd4dd0b2
Ghoya <canadamubarak1@gmail.com>
2020-03-04 10:07:49 -0800

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -11,12 +11,23 @@ public class BasicNavigation {
         // setup webdriver( browser driver) and create web driver object
         WebDriverManager.chromedriver().setup();//
 // ChromeDriver driver = new ChromeDriver();
-     //   WebDriver is interface for Different drivers
- //       RemoteWebDriver  driver= new ChromeDriver();
-        WebDriver driver= new ChromeDriver();// we will use Chrom
+        //   WebDriver is interface for Different drivers
+        //       RemoteWebDriver  driver= new ChromeDriver();
+        WebDriver driver = new ChromeDriver();// we will use Chrom
 // in Selenium every thing began with WebDriver interface
         driver.get("http://google.com"); // to open a webside
         Thread.sleep(3000);// for demo , waite 3 seconds
-driver.close();
+        // method that return page title
+        // you can also see it as tab name , in the browser
+        String title = driver.getTitle(); // returns title
+        String expectedTitle = "Google";
+        System.out.println("Google title is..... = " + title);
+        if (expectedTitle.equals(title)) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+            driver.close();// to close browser
+        }
+            // browser cann't close it self
+        }
     }
-}
