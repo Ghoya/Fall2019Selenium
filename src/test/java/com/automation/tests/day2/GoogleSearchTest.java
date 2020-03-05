e8ced1d88a2e6d73e3e1a19247301383839c2487
Ghoya <canadamubarak1@gmail.com>
2020-03-05 11:34:12 -0800

    created basic navigation class

--- src/test/java/com/automation/tests/day2/GoogleSearchTest.java
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -13,14 +13,23 @@ public class GoogleSearchTest {
         WebDriver driver= new ChromeDriver();
         driver.get("http://google.com");
 Thread.sleep(3000);
+// By.name("q") -->name is one of the selenium locators
+        // there are 8 lacators and we use lacators to find elements
+      // to choose locator we use By.locator
         WebElement search= driver.findElement(By.name("q"));
         //once we found an element how to enter text?
         // to enter text use sendKeys() method
         // how to press enter after entering the text?
         // use Key.ENTER---perform keyboard click
-       search.sendKeys("Java", Keys.ENTER);
-        Thread.sleep(3000);
-driver.quit();
+        //keysToSend---name of the parameter, we don't specify it
+       search.sendKeys("ghoya", Keys.ENTER);
+        Thread.sleep(4000);
+       // if see <a> element , it calls  link
+        // visible text of this link, can be used by selenium to find this element
+      WebElement news=driver.findElement(By.linkText("News"));
+news.click();
+        Thread.sleep(4000);
+driver.quit();// to click on the element
 
 
     }
