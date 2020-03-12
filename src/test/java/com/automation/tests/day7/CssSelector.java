1f42fdf56f490573c7815a9816368dbfa04bfe53
Ghoya <canadamubarak1@gmail.com>
2020-03-12 07:56:36 -0700

    created basic navigation class

--- src/test/java/com/automation/tests/day7/CssSelector.java
+++ src/test/java/com/automation/tests/day7/CssSelector.java
@@ -12,17 +12,25 @@ public class CssSelector {
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
         BrowserUtils.wait(3);
    WebElement heading = driver.findElement(By.cssSelector(".h3"));
-        WebElement home = driver.findElement(By.cssSelector(".nav_link"));
+        WebElement home = driver.findElement(By.cssSelector(".nav-link"));
         WebElement button1 = driver.findElement(By.cssSelector("[onclick='button1()']"));
+        button1.click();
+        BrowserUtils.wait(2);
         WebElement button2 = driver.findElement(By.cssSelector("[name='button2()']"));
-         WebElement button3 = driver.findElement(By.cssSelector("[onclick='button']"));
-                WebElement button4 = driver.findElement(By.cssSelector("[onclick='button']"));
-BrowserUtils.wait(2);
-button1.click();
         button2.click();
+        BrowserUtils.wait(2);
+        WebElement button3 = driver.findElement(By.cssSelector("[id^='button_']"));
         button3.click();
+        BrowserUtils.wait(2);
+ WebElement button4 = driver.findElement(By.cssSelector("[onclick='button4()']"));
         button4.click();
-
+        BrowserUtils.wait(2);
+        WebElement button5 = driver.findElement(By.cssSelector("[name='button5()']"));
+        button5.click();
+        BrowserUtils.wait(2);
+        WebElement button6 = driver.findElement(By.cssSelector("#disappearing_button"));
+        button6.click();
+        BrowserUtils.wait(2);
 
 driver.close();
     }
