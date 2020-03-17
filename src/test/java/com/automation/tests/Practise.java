b2e16948c4c3fd2c98816c9ea880febf510818eb
Ghoya <canadamubarak1@gmail.com>
2020-03-16 20:25:45 -0700

    created basic navigation class

--- src/test/java/com/automation/tests/Practise.java
+++ src/test/java/com/automation/tests/Practise.java
@@ -1,21 +1,44 @@
 package com.automation.tests;
 
 
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Practise {
-  @Test
-  public void test1(){
-      String expected="imam";
-      String actual= "imam";
-      Assert.assertEquals(actual,expected);
-  }
-  @Test
-public void test2(){
-      int num1=4;
-      int num2=30;
-      Assert.assertTrue(num1>num2);
-}
+import java.util.List;
 
+public class Practise {
+private WebDriver driver;
+@Test
+public void googleSearchTest(){
+    driver.get("http://google.com");
+    driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
+    BrowserUtils.wait(3);
+    List<WebElement> searchItems=driver.findElements(By.tagName("h3"));
+    for (WebElement searchItem:searchItems) {
+       String var= searchItem.getText();
+       if(!var.isEmpty()){
+           System.out.println(var);
+       //   Assert.assertTrue(var.toLowerCase().contains("java"));
+       }
+    }
+}
+@BeforeMethod
+    public void setUp() {
+
+      driver= DriverFactory.createADriver("chrome");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+   driver.quit();
+    }
 }
