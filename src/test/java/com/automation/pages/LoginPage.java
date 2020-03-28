d14e4c4298ec52dc99acee5a65b0d84ac61bd15c
Ghoya <canadamubarak1@gmail.com>
2020-03-28 10:40:32 -0700

    created basic navigation class

---
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package com.automation.pages;
+
+import com.automation.utilities.ConfigurationReader;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+   @FindBy(id="prependedInput")
+public WebElement userName;
+  @FindBy(id="prependedInput2")
+public WebElement password;
+@FindBy(id="_submit")
+public WebElement login;
+@FindBy(linkText = "Forgot your password?")
+public WebElement forgotPassword;
+
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+public void login(String userNameValue, String passwordValue){
+   userName.sendKeys(userNameValue);
+password.sendKeys(passwordValue);
+    }
+public void login(){
+    userName.sendKeys(ConfigurationReader.getProperty("store_manager"));
+password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+    }
+
+
+}
